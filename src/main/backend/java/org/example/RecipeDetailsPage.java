36daa8aaa87eea1ec4fdd5bce08b521aedbb6429
mayasahota <mayasahota@hotmail.co.uk>
2024-12-10 15:24:42 +0000

    Implementing ingredients list
    I have added a section to the recipe details page backend that outputs the ingredients used for the recipe to the terminal

--- src/main/backend/java/org/example/RecipeDetailsPage.java
+++ src/main/backend/java/org/example/RecipeDetailsPage.java
@@ -27,7 +27,18 @@ public class RecipeDetailsPage {
                     System.out.println("Instructions:");
                     System.out.println(rs.getString("recipe_steps"));
                 }
-                //insert here
+                String recipe_ingredients_sql= "SELECT recing.quantity AS quantity_needed,fridgecont.ingredient_quantity AS fridge_quantity, ing.ingredient_name, ing.ingredient_units FROM recipe rec JOIN recipe_ingredients recing ON rec.recipe_ID = recing.recipe_ID JOIN ingredients ing ON recing.ingredient_ID = ing.ingredient_ID JOIN fridge_contents fridgecont ON fridgecont.fridge_ingredient_ID = ing.ingredient_ID WHERE recipe_name = ? AND fridgecont.student_ID=?;";
+                try (PreparedStatement pstmt2 = conn.prepareStatement(recipe_ingredients_sql)) {
+                    pstmt2.setString(1, current_recipe_name);
+                    pstmt2.setInt(2, current_student_ID);
+                    rs = pstmt2.executeQuery();
+                    //this section is for outputting the recipe ingredients
+                    System.out.println("Ingredients:");
+                    while (rs.next()){
+                        System.out.println(rs.getString("quantity_needed")+rs.getString("ingredient_units")+" "+ rs.getString("ingredient_name"));
+                    }
+                }
+
             }
         }
         catch (SQLException e) {
