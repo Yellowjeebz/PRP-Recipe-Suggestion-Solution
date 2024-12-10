8a03634fd07a8a16f6092ffbb331588cdfe87a8f
mayasahota <mayasahota@hotmail.co.uk>
2024-12-10 15:01:56 +0000

    Renaming SQL file
    I have renamed the SQL file to clarify that it stores all the SQL statements used for the system not just the recipe selection ones

--- src/main/database/sql_statements.sql
+++ src/main/database/sql_statements.sql
@@ -19,4 +19,13 @@ SELECT recipe_name, recipe_ID FROM recipe WHERE recipe_ID=current_complete_recip
 SELECT recipe_name, recipe_ID FROM recipe WHERE recipe_ID=current_semi_recipe;
 
 --this statement will select the information to be displayed in the fridge contents part
-SELECT fridgecont.fridge_ingredient_ID, ing.ingredient_name, fridgecont.ingredient_quantity, fridgecont.ingredient_date FROM fridge_contents fridgecont JOIN ingredients ing ON fridgecont.fridge_ingredient_ID = ing.ingredient_ID WHERE fridgecont.student_ID=current_student_ID;
+SELECT fridgecont.fridge_ingredient_ID,ing.ingredient_units,ing.ingredient_name, fridgecont.ingredient_quantity, fridgecont.ingredient_date FROM fridge_contents fridgecont JOIN ingredients ing ON fridgecont.fridge_ingredient_ID = ing.ingredient_ID WHERE fridgecont.student_ID=current_student_ID;
+
+--these statements will select the information for the recipe details page
+--current_recipe_name
+
+--to get recipe steps
+SELECT recipe_steps, recipe_name FROM recipe WHERE recipe_name=current_recipe_name;
+
+--to get ingredient
+SELECT recing.quantity AS quantity_needed,fridgecont.ingredient_quantity AS fridge_quantity, ing.ingredient_name, ing.ingredient_units FROM recipe rec JOIN recipe_ingredients recing ON rec.recipe_ID = recing.recipe_ID JOIN ingredients ing ON recing.ingredient_ID = ing.ingredient_ID JOIN fridge_contents fridgecont ON fridgecont.fridge_ingredient_ID = ing.ingredient_ID WHERE recipe_name = current_recipe_name;
