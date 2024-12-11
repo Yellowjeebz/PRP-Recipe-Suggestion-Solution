3572f3d8bff41546f1280ef23c2ab0ccca28299b
LRT2408 <lrthornley24@outlook.com>
2024-12-11 15:31:29 +0000

    edit to error catching

--- src/main/java/org/example/service/FridgeContentsService.java
+++ src/main/java/org/example/service/FridgeContentsService.java
@@ -42,7 +42,7 @@ public class FridgeContentsService {
 
             return fridgeEntries;
         } catch (Exception e) {
-            System.err.println(e.getMessage());
+            System.out.println(e.getMessage());
             return new ArrayList<>();
         }
     }
